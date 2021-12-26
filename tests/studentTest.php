9077023b01e99ce605b69ad3ccb7f5d7c70b50c8
Your Name <you@example.com>
2021-12-26 18:44:00 +0100

    update studentTest

--- tests/studentTest.php
+++ tests/studentTest.php
@@ -4,9 +4,5 @@
     require "bootstrap.php";
     require "../src/studentClass.php";
 
-    $student1 = new Student("Jan","Kovář","051125/1025",["M"=>3,"CJ"=>2,"PJT"=>1,"ANJ"=>3,"IoT"=>2]);
-
-    Assert::same("men",$student1->getGender());
-    //Assert::true($student1->isGirl());
-    Assert::true($student1->isBoy());
+    
 ?>
